245953ef320681d65ce4188c252ba6cb249e5624
anupamjhabbl <anupamimpbbl@gmail.com>
2023-08-15 16:30:44 +0530

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
         blueblack:"#181c2e",
         section5blue_start:'#1c6bc1',
         section5blue_end:"#1f70c5",
-        plane:"#135bb7"
+        plane:"#135bb7",
+        whiteblue:"#f5f8fe",
+        merchants:"#f3f7ff"
       }
     },
   },
